7bffed816e30f32b15bed663998914dae309900a
Dan <danrix89@gmail.com>
2017-02-20 15:25:08 -0700

    Added an Interface (MenuReturnable) instead of "Activity" superclass that we had in the UML because this is much more practical.

---
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/MenuReturnable.java
@@ -0,0 +1,15 @@
+package com.example.cs246project.kindergartenprepapp;
+
+import android.view.View;
+
+/**
+ * An interface for ensuring that the implementer returns to the Main Menu.
+ * <p>
+ * @author  Dan Rix
+ * @version 1.0
+ * @since   2017-02-20
+ */
+
+public interface MenuReturnable {
+    public void returnToMainMenu(View view);
+}
