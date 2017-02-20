c3177a3a8c226cd714ebd701b551b5e78d4fa9c0
Dan <danrix89@gmail.com>
2017-02-20 15:28:47 -0700

    Added a class comment to the MainActivity.

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/MainActivity.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.cs246project.kindergartenprepapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * The main (menu) Activity where the user can select an tracing or selection "activity" to help
+ * them prepare for Kindergarten by enhancing their reading, writing, and comprehension skills.
+ * <p>
+ * @author  Micheal Lucero, Trevor Adams, & Dan Rix
+ * @version 1.0
+ * @since   2017-02-20
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
