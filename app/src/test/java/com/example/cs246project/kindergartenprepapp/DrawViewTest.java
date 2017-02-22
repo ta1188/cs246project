0daa99602207f4a597755a1c0fe16b203a82fd6e
Dan <danrix89@gmail.com>
2017-02-22 12:41:03 -0700

    Added a test class (DrawViewTest) for testing DrawView. This is to accomplish the week 08 team activity.

---
+++ app/src/test/java/com/example/cs246project/kindergartenprepapp/DrawViewTest.java
@@ -0,0 +1,26 @@
+package com.example.cs246project.kindergartenprepapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dan on 2/22/2017.
+ */
+public class DrawViewTest {
+    @Test
+    public void clearView() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void onTouchStart() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void onTouchMove() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+}
