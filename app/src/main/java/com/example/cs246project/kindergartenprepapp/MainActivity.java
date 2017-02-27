2dfb4b1c4e8cc2a7e7e306be65c6342160249f86
Trevor Adams <tsadams1@gmail.com>
2017-02-27 15:01:14 -0700

    I have added the WordSelectable activity. There is a placeholder button on the main view to access the activity. There are a number of features to still implement, however it will dynamically add buttons based on an array of filenames given, and assign a unique id to each.

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/MainActivity.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cs246project.kindergartenprepapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * The main (menu) Activity where the user can select an tracing or selection "activity" to help
@@ -18,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendToWordSelectable(View view) {
+        Intent intent = new Intent(getBaseContext(), WordSelectable.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/WordSelectable.java
@@ -0,0 +1,49 @@
+package com.example.cs246project.kindergartenprepapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public class WordSelectable extends AppCompatActivity {
+
+    // Create a new Array list that will hold the filenames to reference
+    ArrayList<String> myImages = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_selectable);
+
+        // This is just an example of images for the array
+        // In the app these would be passed in or randomly generated
+        myImages.add("ic_3");
+        myImages.add("ic_3");
+        myImages.add("ic_3");
+
+        // Find the horizontal scroll view
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+
+        /*
+        * This will loop through our image array based on the length of the array
+        * It will then get the index of each file in the drawable directory,
+        * then it will update the image for each button.
+        * */
+        for (int i = 0; i < myImages.size(); i++) {
+            int index = this.getResources().getIdentifier(myImages.get(i), "drawable", this.getPackageName());
+            ImageButton imageButton = new ImageButton(this);
+            imageButton.setId(i);
+            imageButton.setImageResource(index);
+            layout.addView(imageButton);
+        }
+    }
+
+    /* TO DO...
+    *  - Add on click handlers for each button dynamically
+    *  - Update display of clicked button to not display
+    *       - Possibly remove it from the view altogether...?
+    * */
+
+}
