6144a6f97235b685b65fa7e0932f86cb2882fb9c
Trevor Adams <tsadams1@gmail.com>
2017-03-09 16:12:39 -0700

    Updated layout on name entry (activity_main). Basic files for selectable (NOT updated based on our conversation on 3/9/17 yet).

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/WordSelectable.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/WordSelectable.java
@@ -1,48 +1,143 @@
 package com.example.cs246project.kindergartenprepapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class WordSelectable extends AppCompatActivity {
+public class WordSelectable extends AppCompatActivity implements View.OnTouchListener {
+
+    /* Parameters:
+        * Factory:
+        *   Type
+        *   ArrayList of answers
+        *   Number of buttons
+        *   Context
+        *
+        * If name...
+        *   Type
+        *   LetterArray
+        *   Context
+    * */
 
     // Create a new Array list that will hold the filenames to reference
     ArrayList<String> myImages = new ArrayList<String>();
+
+    // Find the horizontal scroll view
+    LinearLayout layout;
+
+    ProgressBar _progBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_selectable);
+        layout = (LinearLayout) findViewById(R.id.layout_word);
+        _progBar = (ProgressBar) findViewById(R.id.progressBar2);
 
         // This is just an example of images for the array
         // In the app these would be passed in or randomly generated
-        myImages.add("ic_3");
-        myImages.add("ic_3");
+        myImages.add("upper_r");
+        myImages.add("upper_h");
+        myImages.add("upper_j");
+        myImages.add("upper_t");
 
-        // Find the horizontal scroll view
-        LinearLayout layout = (LinearLayout) findViewById(R.id.layout_word);
+        viewSetUp();
 
-        /*
-        * This will loop through our image array based on the length of the array
+        setMainImage();
+    }
+
+    public void viewSetUp() {
+         /*
+        * This will loop through the returned array based on the length of the array
         * It will then get the index of each file in the drawable directory,
         * then it will update the image for each button.
         * */
         for (int i = 0; i < myImages.size(); i++) {
             int index = this.getResources().getIdentifier(myImages.get(i), "drawable", this.getPackageName());
             ImageButton imageButton = new ImageButton(this);
-            imageButton.setId(i);
+            if (i == 0) {
+                imageButton.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            _progBar.incrementProgressBy(1);
+                            Log.d("WordSelectable", "------- CORRECT --------");
+                            CharSequence text = "Correct!";
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+
+                            resetActivity();
+                        }
+                        return false;
+                    }
+                });
+            } else {
+                imageButton.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            Log.d("WordSelectable", "------- WRONG --------");
+                            CharSequence text = "Incorrect!";
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                        }
+                        return false;
+                    }
+                });
+            }
+
             imageButton.setImageResource(index);
             layout.addView(imageButton);
+
         }
     }
 
-    /* TO DO...
-    *  - Add on click handlers for each button dynamically
-    *  - Update display of clicked button to not display
-    *       - Possibly remove it from the view altogether...?
-    * */
+    public void setMainImage() {
+        // Parse the first element for letter to match
+        String answerImg = myImages.get(0).substring(myImages.get(0).length() - 1);
+        // Structure filename of object
+        answerImg = "object_" + answerImg;
+
+        // Grab the image resource and set the image drawable
+        int imageId = this.getResources().getIdentifier(answerImg, "drawable", this.getPackageName());
+        Drawable res = getResources().getDrawable(imageId, getTheme());
+        ImageView imageView = (ImageView) findViewById(R.id.objectImage);
+        imageView.setImageDrawable(res);
+    }
+
+
+    public void resetActivity() {
+        layout.removeAllViews();
+        myImages.clear();
+        myImages.add("upper_b");
+        myImages.add("upper_g");
+        myImages.add("upper_i");
+        myImages.add("upper_a");
+
+        setMainImage();
+        viewSetUp();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
 
 }
