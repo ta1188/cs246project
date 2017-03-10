1a741da329a963885fa4a0bcab0f5ad3b5742d40
Dan <danrix89@gmail.com>
2017-03-09 22:54:35 -0700

    Implemented MediaButton and MediaModel that will be used in Selectable activities.

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/MediaButton.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/MediaButton.java
@@ -1,57 +1,106 @@
 package com.example.cs246project.kindergartenprepapp;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.widget.ImageButton;
+import android.view.View;
+import java.io.IOException;
 
 /**
- * Created by Michael Lucero on 3/3/17.
+ * An Image Button that plays audio when clicked/touched.
+ * <p>
+ * @author  Michael Lucero & Dan Rix
+ * @version 1.0
+ * @since   2017-03-09
  */
 
+public class MediaButton<T> extends android.support.v7.widget.AppCompatImageButton {
 
-public class MediaButton extends ImageButton implements MediaPlayer.OnCompletionListener {
+    // The media player used to play the audio when clicked
+    private MediaPlayer _mediaPlayer;
 
-    private MediaPlayer mediaPlayer;
-    private Integer _buttonName;
-    private Boolean isAnswer;
-    private String _imageFile;
-    private String[] array;
+    // Thee model object that handles image/audio resources
+    private MediaModel<T> _model;
 
-    public MediaButton(String activityType, Context context, Integer buttonName, Boolean isAnswer) {
+    /**
+     * Non-Default Constructor
+     * Constructs an instance of MediaButton with a model object for managing all of the image and
+     * audio file resources.
+     * @param context of the application
+     * @param model used for managing resources
+     */
+    public MediaButton(Context context, MediaModel<T> model) {
         super(context);
+        _model = model;
 
+        // Set the image of the button
+        setImageResource(_model.getImageFileResourceIndex());
 
-
+        // Setup to play audio files when clicked/touched
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAudio();
+            }
+        });
     }
 
-    /*int assciiValue;
-    char myChar;
-
-    assciiValue = questionBank.get(_buttonName);
-    myChar = (char) letter;
-
-    // adds
-    _soundResourceFile = Uri.parse("android.resource://com.example.cs246project." +
-            "kindergartenprepapp/raw/" + audioFileName);
-
-    // int a = R.drawable.raccoon;
-    imageFileName = "android.resource://com.example.cs246project.kindergartenprepapp/drawable/" + imageFileName;
-    _imageResourceFile = Drawable.createFromPath(imageFileName);
-
-    // try
-    _mediaPlayer = MediaPlayer.create(context, _soundResourceFile);
-    _imageButton = new ImageButton(context);
-    */
-
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-
+    /**
+     * Get Value
+     * Gets the "value" associated with the model.
+     * @return the model's value.
+     */
+    public T getValue() {
+        return _model.getValue();
     }
 
-    //_imageButton.setBackground(_imageResourceFile);
+    /**
+     * Play Audio
+     * Plays any/all audio resources stored in the model.
+     */
+    private void playAudio() {
+        // Only play audio if you have it.
+        if (_model.hasAudio()) {
+            // Initialize the media player with the first audio resource
+            _mediaPlayer = MediaPlayer.create(getContext(), _model.getAudioSourceIndex());
 
+            // Set the on completion listener to play all other audio resources when each is complete
+            _mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    if(!_model.isAtEndOfAudio())
+                    {
+                        // Reset the media player
+                        _mediaPlayer.reset();
 
+                        // Load the media player with a new audio resource
+                        try {
+                            AssetFileDescriptor afd = getContext().getResources().openRawResourceFd(_model.getAudioSourceIndex());
+                            if (afd == null) return;
+                            _mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                            afd.close();
+                            _mediaPlayer.prepare();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Play the audio
+                        _mediaPlayer.start();
+                    }
+                    else
+                    {
+                        // Release the media player
+                        _mediaPlayer.release();
+
+                        // Reset the index to point back to the first audio file
+                        _model.resetAudioIndex();
+                    }
+                }
+            });
+
+            // Play the first audio track
+            _mediaPlayer.start();
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/MediaModel.java
@@ -0,0 +1,96 @@
+package com.example.cs246project.kindergartenprepapp;
+
+import java.util.List;
+
+/**
+ * A class for holding and managing image/audio file references.
+ * <p>
+ * @author  Dan Rix
+ * @version 1.0
+ * @since   2017-03-09
+ */
+
+public class MediaModel<T> {
+
+    // The "drawable" resource index of an image file
+    private int _imageFileResourceIndex;
+
+    // The "raw" resources indexes of audio files
+    private List<Integer> _audioFileResourceIndexes;
+
+    // The current index of an audio resource
+    private int _currentAudioResourceIndex;
+
+    // The "value" of the object.
+    private T _value;
+
+    /**
+     * Non-Default Constructor
+     * Constructs a MediaModel with image/audio resources and an associated value
+     * @param imageFileResourceIndex used to set _imageFileResourceIndex
+     * @param audioFileResourceIndexes used to set _audioFileResourceIndexes
+     * @param value used to set _value
+     */
+    public MediaModel(int imageFileResourceIndex, List<Integer> audioFileResourceIndexes, T value) {
+        _imageFileResourceIndex = imageFileResourceIndex;
+        _audioFileResourceIndexes = audioFileResourceIndexes;
+        _currentAudioResourceIndex = 0;
+        _value = value;
+    }
+
+    /**
+     * Get Image File Resource Index
+     * Getter for the _imageFileResourceIndex.
+     * @return _imageFileResourceIndex
+     */
+    public int getImageFileResourceIndex() {
+        return _imageFileResourceIndex;
+    }
+
+    /**
+     * Get Value
+     * Getter for the _value.
+     * @return _value
+     */
+    public T getValue() {
+        return _value;
+    }
+
+    /**
+     * Get Audio Source Index
+     * Gets the current audio resource index from _audioFileResourceIndexes and then increments the
+     * _currentAudioResourceIndex so clients use audio resources in succession.
+     * @return the current audio resource index
+     */
+    public int getAudioSourceIndex() {
+        int resourceIndex = _audioFileResourceIndexes.get(_currentAudioResourceIndex);
+        _currentAudioResourceIndex++;
+        return resourceIndex;
+    }
+
+    /**
+     * Reset Audio Index
+     * Set the _currentAudioResourceIndex back to 0.
+     */
+    public void resetAudioIndex() {
+        _currentAudioResourceIndex = 0;
+    }
+
+    /**
+     * Is At End Of Audio
+     * Has the client successively gone through all the audio resources?
+     * @return if the _currentAudioResourceIndex has reached the end
+     */
+    public boolean isAtEndOfAudio() {
+        return _currentAudioResourceIndex >= _audioFileResourceIndexes.size();
+    }
+
+    /**
+     * Has Audio
+     * Flag to know if there are any audio resources at all.
+     * @return if there is audio resources or not.
+     */
+    public boolean hasAudio() {
+        return _audioFileResourceIndexes.size() > 0;
+    }
+}
