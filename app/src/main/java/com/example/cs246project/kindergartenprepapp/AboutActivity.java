6e2a334481df8b5aeb5f47c39acb661a33748abb
Trevor Adams <tsadams1@gmail.com>
2017-03-15 09:11:42 -0600

    Added updated image for app icon, activity icons, and added ‘zero’ svg. Updated about page to include back button.

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/AboutActivity.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/AboutActivity.java
@@ -2,6 +2,7 @@ package com.example.cs246project.kindergartenprepapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void returnToMenu(View view) {
+        this.finish();
     }
 }
