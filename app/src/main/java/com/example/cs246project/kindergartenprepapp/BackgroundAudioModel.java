b4f2b2cedd9def0638f3e17eae10755687a37a3d
Michael Lucero <luc14006@byui.edu>
2017-03-22 13:50:39 -0600

    Fixed issues with the background audio playing during activties only. Fixed issue with the audio breaking when jumping back and forth from activities. Done by managing activity life cycle.

---
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/BackgroundAudioModel.java
@@ -0,0 +1,61 @@
+package com.example.cs246project.kindergartenprepapp;
+
+        import android.content.Context;
+        import android.media.MediaPlayer;
+        import android.util.Log;
+
+        import java.io.IOException;
+
+/**
+ * Created by Michael Lucero on 3/21/17.
+ */
+
+public class BackgroundAudioModel {
+
+    private Boolean _isBackgroundAudioPlayable = true;
+    private MediaPlayer _themeMediaPlayer;
+
+    public BackgroundAudioModel(Context context) {
+        startBackgroundAudio(context);
+    }
+
+    public boolean isPlaying() {
+        return _themeMediaPlayer.isPlaying();
+    }
+
+    public void startBackgroundAudio(Context context) {
+        _themeMediaPlayer = MediaPlayer.create(context, R.raw.skiptap_theme);
+        _themeMediaPlayer.setLooping(true);
+        _themeMediaPlayer.setVolume(0.2f, 0.2f);
+        _themeMediaPlayer.start();
+    }
+
+    public void stopBackgroundAudio() {
+        if (_themeMediaPlayer != null) {
+            if (_themeMediaPlayer.isPlaying() && _isBackgroundAudioPlayable) {
+                _themeMediaPlayer.setLooping(false);
+                _themeMediaPlayer.stop();
+                _themeMediaPlayer.release();
+                _themeMediaPlayer = null;
+            }
+        }
+    }
+
+    public void disableAudio(Boolean isBackgroundAudioPlayable) {
+        _isBackgroundAudioPlayable = isBackgroundAudioPlayable;
+    }
+
+
+    public void pauseBackgroundAudio() {
+        if (_themeMediaPlayer.isPlaying()) {
+            _themeMediaPlayer.pause();
+        }
+
+    }
+
+    public void resumeBackgroundAudio() {
+        if (_themeMediaPlayer.isPlaying()) {
+            _themeMediaPlayer.start();
+        }
+    }
+}
--- app/src/main/java/com/example/cs246project/kindergartenprepapp/SkipTapActivity.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/SkipTapActivity.java
@@ -1,8 +1,13 @@
 package com.example.cs246project.kindergartenprepapp;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
 
 /**
  * The super Activity of all SkipTap activities which has common member variables and member
@@ -15,11 +20,20 @@ import android.support.v7.app.AppCompatActivity;
 
 abstract class SkipTapActivity extends AppCompatActivity {
 
+    protected BackgroundAudioModel _backgroundAudioModel;
     protected MediaPlayer _instructionsMediaPlayer;
+    protected int _instructionsAudioResourceIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // For hiding status the bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        _backgroundAudioModel = new BackgroundAudioModel(this);
     }
 
     /**
@@ -27,6 +41,14 @@ abstract class SkipTapActivity extends AppCompatActivity {
      * @param instructionsAudioResourceIndex Audio resource index of the instruction to be played.
      */
     protected void playInstructions(int instructionsAudioResourceIndex) {
+
+        // keep track for onResume
+        _instructionsAudioResourceIndex = instructionsAudioResourceIndex;
+
+        if (_instructionsMediaPlayer != null) {
+            _instructionsMediaPlayer.release();
+        }
+
         // Initialize a media player with the audio resource
         _instructionsMediaPlayer = MediaPlayer.create(this, instructionsAudioResourceIndex);
 
@@ -34,6 +56,7 @@ abstract class SkipTapActivity extends AppCompatActivity {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 _instructionsMediaPlayer.release();
+                _instructionsMediaPlayer = null;
                 onInstructionsAudioComplete();
 
             }
@@ -47,4 +70,67 @@ abstract class SkipTapActivity extends AppCompatActivity {
         // Do nothing here.
         // Descendants may Override this if they want (not abstract == not required)
     }
+
+    /**
+     * Will kill the media player
+     */
+    public void stopAudio() {
+        if (_instructionsMediaPlayer != null) {
+            _instructionsMediaPlayer.release();
+            _instructionsMediaPlayer = null;
+        }
+
+        _backgroundAudioModel.stopBackgroundAudio();
+    }
+
+    /**
+     * Will
+     */
+    public void startAudio() {
+
+        playInstructions(_instructionsAudioResourceIndex);
+
+        _backgroundAudioModel.startBackgroundAudio(this);
+    }
+
+    @Override
+    protected void onStop() {
+        stopAudio();
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        stopAudio();
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+
+        // For hiding status the bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        if(!_backgroundAudioModel.isPlaying()) {
+            startAudio();
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // For hiding status the bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
 }
+
+
