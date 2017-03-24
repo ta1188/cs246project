be6896a2fdfc01e3db0a4bd5043fa86795e92878
Michael Lucero <luc14006@byui.edu>
2017-03-23 21:30:55 -0600

    Added button for turning on and off music

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/AboutActivity.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/AboutActivity.java
@@ -10,9 +10,36 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // For hiding status the bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     public void returnToMenu(View view) {
         this.finish();
+    }
+
+    @Override
+    protected void onResume() {
+
+        // For hiding status the bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+
+        // For hiding status the bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        super.onRestart();
     }
 }
