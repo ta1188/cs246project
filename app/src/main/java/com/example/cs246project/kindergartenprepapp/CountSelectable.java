26aed700f0298f77b5b161dcbdc7b48462a946e3
Michael Lucero <luc14006@byui.edu>
2017-03-27 04:12:11 -0600

    Updated all toast display timing to be connected with audio playing rather then a timer based on moving toasts to the skip tap activity from selectable model.

--- app/src/main/java/com/example/cs246project/kindergartenprepapp/CountSelectable.java
+++ app/src/main/java/com/example/cs246project/kindergartenprepapp/CountSelectable.java
@@ -32,9 +32,6 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
     Context context = this;
     private ImageView imageView;
 
-    // Toast instructions should play the length of the audio instructions
-    final private int toastTimeLength = 2000;
-
     int count = 1;
 
     @Override
@@ -62,8 +59,9 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
         enableMainImageButton(false);
         disableQuestionButtons(true);
 
+        // show toast for the duration of the instructions and then stop when audio is over
+        displayInstructionToast();
         playInstructions(_model.getActivityInstructionsIndex());
-        _model.displayInstructionToast(toastTimeLength);
     }
 
     public void viewSetUp() {
@@ -103,10 +101,10 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
                 wasTrue = true;
                 _progBar.incrementProgressBy(1);
                 // Show answer toast
-                _model.displayToast(true);
+                displayToast(true);
             } else {
                 // Show answer toast
-                _model.displayToast(false);
+                displayToast(false);
             }
         }
     }
@@ -157,6 +155,12 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
         if (_model._isActivityDone) {
             this.finish();
         } else {
+
+            // stop the correct/incorrect toast when done
+            if (_toast != null) {
+                _toast.cancel();
+            }
+
             // Enable the buttons when sound is complete
             disableQuestionButtons(false);
             enableMainImageButton(true);
@@ -171,15 +175,22 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
      */
     @Override
     public void onInstructionsAudioComplete() {
+
+        // stop the instructions toast when done
+        if (_toast != null) {
+            _toast.cancel();
+        }
+
         // Enable the buttons when sound is complete
         disableQuestionButtons(false);
 
         isFirstTime = false;
+
     }
 
     public void returnToMenu(View view) {
-        if(_model._toast != null) {
-            _model.cancelToast();
+        if (_toast != null) {
+            _toast.cancel();
         }
 
         this.finish();
@@ -191,13 +202,14 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
      */
     @Override
     public void stopEverything() {
+        // cancel any toasts that are still showing; done in override
+        // leaving app
+        if (_toast != null) {
+            _toast.cancel();
+        }
 
         // stop all audio that is playing
         stopAudio();
-
-        // cancel any toasts that are still showing; done in override
-        // leaving app so
-        _model.cancelToast();
     }
 
     /**
@@ -209,7 +221,7 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
 
         for (int i = 0; i < layout_top.getChildCount(); i++) {
             MediaButton mediaButton = (MediaButton) layout_top.getChildAt(i);
-            mediaButton.stopAudio();
+            mediaButton.stopAudio(); // media button stop audio does release
         }
         for (int i = 0; i < layout_bottom.getChildCount(); i++) {
             MediaButton mediaButton = (MediaButton) layout_bottom.getChildAt(i);
@@ -230,214 +242,8 @@ public class CountSelectable extends SkipTapActivity implements View.OnTouchList
         disableQuestionButtons(true);
         playInstructions(_instructionsAudioResourceIndex);
 
-        _model.displayInstructionToast(toastTimeLength);
+        displayInstructionToast();
 
         _backgroundAudioModel.startBackgroundAudio(this);
     }
-
-
-
-//
-//
-//    // Create a new Array list that will hold the filenames to reference
-//    private CountSelectableModel _model;
-//
-//    // Find the horizontal scroll view
-//    private LinearLayout layout_top;
-//    private LinearLayout layout_bottom;
-//    private ProgressBar _progBar;
-//    private boolean wasTrue = false;
-//    private ImageView imageView;
-//    private MediaPlayer mainImageMediaPlayer;
-//
-//    int count = 1;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.count_selectable);
-//        layout_top = (LinearLayout) findViewById(R.id.layout_count_top);
-//        layout_bottom = (LinearLayout) findViewById(R.id.layout_count_bottom);
-//        _progBar = (ProgressBar) findViewById(R.id.progressBarCount);
-//        _model = new CountSelectableModel(this, 4);
-//        playInstructions(_model.getActivityInstructionsIndex());
-//
-//        viewSetUp();
-//        imageView = (ImageView) findViewById(R.id.countImage);
-//        imageView.setEnabled(false);
-//        setMainImage();
-//        // Disable the buttons
-//        enableDisableButtons(true);
-//    }
-//
-//    public void viewSetUp() {
-//        /**
-//         * This will loop through the generatedValueList based on the length of the array
-//         * It will then get the index of each file in the drawable directory,
-//         * then it will update the image for each button.
-//         * */
-//        for (MediaModel item : _model.generateValueList()) {
-//            final MediaButton btn = new MediaButton(this, item, this);
-//            btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
-//            btn.setScaleType(ImageView.ScaleType.CENTER);
-//            btn.setAdjustViewBounds(true);
-//            btn.setPadding(5, 5, 5, 5);
-//            btn.setElevation(10);
-//            ((ViewGroup.MarginLayoutParams) btn.getLayoutParams()).setMargins(10, 5, 30, 5);
-//            btn.setBackgroundColor(Color.parseColor(_model.getBtnColor()));
-//
-//
-//            if (count <= 2) {
-//                layout_top.addView(btn);
-//            } else {
-//                layout_bottom.addView(btn);
-//            }
-//            count++;
-//        }
-//    }
-//
-//    @Override
-//    public void onMediaButtonTouched(MediaButton mediaButton, MotionEvent event) {
-//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//            if(_model.isCorrect(mediaButton.getValue())) {
-//                wasTrue = true;
-//                _progBar.incrementProgressBy(1);
-//                // Show answer toast
-//                _model.displayToast(true);
-//            } else {
-//                // Show answer toast
-//                _model.displayToast(false);
-//            }
-//            // Disable the buttons
-//            enableDisableButtons(true);
-//        }
-//    }
-//
-//    /**
-//     * Will disable or enable the layout buttons
-//     * */
-//    private void enableDisableButtons(Boolean state){
-//        for (int i = 0; i < layout_top.getChildCount(); i++) {
-//            MediaButton mediaButton = (MediaButton) layout_top.getChildAt(i);
-//            mediaButton.setIsDisabled(state);
-//        }
-//        for (int i = 0; i < layout_bottom.getChildCount(); i++) {
-//            MediaButton mediaButton = (MediaButton) layout_bottom.getChildAt(i);
-//            mediaButton.setIsDisabled(state);
-//        }
-//    }
-//
-//
-//    private void setMainImage() {
-//        // Grab the image resource and set the image drawable
-//        Drawable res = getResources().getDrawable(_model.getAnswerResourceIndex(), getTheme());
-//        imageView.setImageDrawable(res);
-//        final Context context = this;
-//
-//        /**
-//         * Setup event listener for main image
-//         * */
-//        imageView.setOnTouchListener(new View.OnTouchListener() {
-//            @RequiresApi(api = Build.VERSION_CODES.N)
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                    MediaPlayer mp = new MediaPlayer();
-//
-//                    // Reset the media player
-//                    mp.reset();
-//
-//                    int soundId = getResources().getIdentifier("instruct_how_many_objects_can_you_count", "raw", getPackageName());
-//
-//                    mp.create(context, soundId);
-//                    // Load the media player with a new audio resource
-//                    try {
-//                        AssetFileDescriptor afd = context.getResources().openRawResourceFd(soundId);
-//                        mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-//                        afd.close();
-//                        mp.prepare();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//
-//                    // Play the audio
-//                    mp.start();
-//                    imageView.setEnabled(false);
-//
-//                    Runnable disableImageClick = new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            imageView.setEnabled(true);
-//                        }
-//                    };
-//                    Handler handler = new Handler();
-//                    handler.postDelayed(disableImageClick, 1000);
-//                }
-//                return false;
-//            }
-//        });
-//        // Enable the buttons
-//        enableDisableButtons(false);
-//    }
-//
-//
-//    private void resetActivity() {
-//        layout_top.removeAllViews();
-//        layout_bottom.removeAllViews();
-//        wasTrue = false;
-//        count = 1;
-//        viewSetUp();
-//        setMainImage();
-//    }
-//
-//    @Override
-//    public boolean onTouch(View v, MotionEvent event) {
-//        return false;
-//    }
-//
-//    /**
-//     * Called when audio has completed (for media buttons only)
-//     * */
-//    @Override
-//    public void onAudioComplete() {
-//        if (_model._isActivityDone) {
-//            this.finish();
-//        } else {
-//            // Enable the buttons when sound is complete
-//            enableDisableButtons(false);
-//            // check for true
-//            if (wasTrue)
-//                resetActivity();
-//        }
-//    }
-//
-//    @Override
-//    public void onInstructionsAudioComplete() {
-//        imageView.setEnabled(true);
-//        // Enable the buttons when sound is complete
-//        enableDisableButtons(false);
-//    }
-//
-//    public void returnToMenu(View view) {
-//        this.finish();
-//    }
-//
-//    /**
-//     * Handles stopping audio the buttons might be playing when transitioning to other activities
-//     * or moving to another app.
-//     */
-//    @Override
-//    public void stopAudio() {
-//        super.stopAudio();
-//        for (int i = 0; i < layout_top.getChildCount(); i++) {
-//            MediaButton mediaButton = (MediaButton) layout_top.getChildAt(i);
-//            mediaButton.stopAudio();
-//        }
-//        for (int i = 0; i < layout_bottom.getChildCount(); i++) {
-//            MediaButton mediaButton = (MediaButton) layout_bottom.getChildAt(i);
-//            mediaButton.stopAudio();
-//        }
-//    }
-
 }
